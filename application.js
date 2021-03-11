92d5e897fc604531de4456adec04a07302ecbde4
jmdn1g17 <jmdn1g17@soton.ac.uk>
2021-03-11 18:32:54 +0000

    Added basic sparql queries

--- application.js
+++ application.js
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+function test()
+{
+    var returnData;
+    $.ajax({
+      type: "GET",
+      url: "https://raw.githubusercontent.com/Jacob-Neale/Jacob-Neale.github.io/main/resources/data.ttl",
+      dataType: "text",
+      async: false,
+      success: function(response)
+      {
+        returnData = response;
+      }
+    });
+    return returnData;
+}
+console.log(select(test(), null, null, "<Manufacturing>"));
